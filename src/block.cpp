282a4a4428be7d6bb81375c5c2908391c5899fe8
Nicholas Gill <mythagel@gmail.com>
2015-11-14 11:28:00 +1100

    Clarify copyright

--- src/block.cpp
+++ src/block.cpp
@@ -1,3 +1,27 @@
+   /************************************************************************
+
+       Copyright 2008 Mark Pictor
+
+   This file is part of RS274NGC.
+
+   RS274NGC is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   RS274NGC is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with RS274NGC.  If not, see <http://www.gnu.org/licenses/>.
+
+   This software is based on software that was produced by the National
+   Institute of Standards and Technology (NIST).
+
+   ************************************************************************/
+
 /* cxxcam - C++ CAD/CAM driver library.
  * Copyright (C) 2013  Nicholas Gill
  * 
